f13c188e983625fc3d720b42542078fd11182f11
Msantra97 <monojsantra8013@gmail.com>
2023-08-28 01:51:33 +0530

    add index page

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,22 @@
 import React, { useState } from 'react';
-
+import Link from "next/link";
 
 
 const Home = () => {
 
 
   return (
-    <div className="flex justify-center h-screen bg-[#917FB3] ">
-      <h1 className='text-4xl ' >Online Chat Application</h1>
-      <button className='btn bg-black h-auto w-4'>
-        Login
-      </button>
-    </div>
+<div className="flex flex-col items-center justify-center h-screen bg-[#917FB3]">
+  <h1 className='text-4xl mb-6'>Online Chat Application</h1>
+  
+  <Link href="/register">
+  <button className='bg-black hover:bg-white text-white hover:text-black font-semibold py-2 px-4 rounded'>
+
+    Register Here
+  </button>
+  </Link>
+</div>
+
   );
 };
 
